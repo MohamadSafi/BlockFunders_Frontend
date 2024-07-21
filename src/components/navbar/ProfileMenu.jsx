d511aefefb3a63a2b6dd3fdb4e52d984399f4b73
billyprivate <101475647+MohamadSafi@users.noreply.github.com>
2024-07-21 17:39:08 +0300

    Navbar Menue Fixes

--- src/components/navbar/ProfileMenu.jsx
+++ src/components/navbar/ProfileMenu.jsx
@@ -37,18 +37,7 @@ const profileMenuItems = [
     label: "My Profile",
     icon: UserCircleIcon,
   },
-  {
-    label: "Edit Profile",
-    icon: Cog6ToothIcon,
-  },
-  {
-    label: "Inbox",
-    icon: InboxArrowDownIcon,
-  },
-  {
-    label: "Help",
-    icon: LifebuoyIcon,
-  },
+
   {
     label: "Sign Out",
     icon: PowerIcon,
